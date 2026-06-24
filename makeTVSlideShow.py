dcd598155465baf2538a1321ee63d2f8c022b458
Jim Mollmann <jim.mollmann@gmail.com>
2026-06-24 18:14:32 -0400

    Start building the label with EXIF data

--- makeTVSlideShow.py
+++ makeTVSlideShow.py
@@ -15,6 +15,7 @@ from pathlib import Path
 import itertools
 import re
 import pprint
+from fractions import Fraction
 
 def adapt_datetime(dt):
     return dt.isoformat(sep=' ').replace('T', ' ')
@@ -97,8 +98,6 @@ class Pictures:
                 self.rotates[slideDir + file] = ' -flop '
         return self.rotates
         
-        
-        
     def getRotates(self, subDir, option):
         rotateBase = self.picRoot + 'Rotate/' + subDir
         rotPath = Path(rotateBase)
@@ -111,27 +110,29 @@ class Pictures:
         fields = {}
         for dir in sorted(filesInDir):
             for file in filesInDir[dir]:
-                labelData = self.buildLabel(dir + file)
+                labelData = self.getLabelData(dir + file)
                 for meta in labelData:
                     fields[meta] = 1 + fields.get(meta, 0)
         for meta in sorted(fields):
             print(f'{fields[meta]:6d} : {meta:s}')
         return
-        
                     
     def buildLabels(self, filesInDir):
+        labels = {}
         for dir in sorted(filesInDir):
             for file in filesInDir[dir]:
-                labelData = self.buildLabel(dir + file)
+                labelData = self.getLabelData(dir + file)
+                labels[dir + file] = self.composeLabel(dir + file, labelData)
                    
-    def buildLabel(self, file):
+    def getLabelData(self, file):
         self.pp = pprint.PrettyPrinter(indent=4, sort_dicts=False)
         cmd = 'identify -verbose "' + self.picRoot + file + '"'
         result = doCmd(cmd)
         kv_regex = re.compile(r"^\s+([\w\s]+):\s*(.*)$")
         metadata = {}
         for line in result.stdout.splitlines():
-            match = kv_regex.match(line.decode('utf-8'))
+            line = line.decode('utf-8').replace('date:', '').replace('exif:', '').replace('jpeg:', '')
+            match = kv_regex.match(line)
             if match:
                 # clean up keys and store values
                 key = match.group(1).strip().lower().replace(" ", "_")
@@ -139,14 +140,64 @@ class Pictures:
                 metadata[key] = value
         return metadata
         #self.pp.pprint(metadata)
+
+    def composeLabel(self, filename, metadata):
+        label = '\n\n\n\n\n\n\n'
+        label += filename.replace('/', '\n') +'\n\n\n'
+        time = None
+        if metadata.get('createdate', False):
+            time = metadata['createdate']
+        elif  metadata.get('filemodifydate', False):
+            time = metadata['filemodifydate']
+        else:
+            time = metadata.get('datetime', None)
+        if time is not None:
+            label += time + '\n\n'
+        if metadata.get('shutterspeedvalue', False):
+            ss = float(Fraction(metadata['shutterspeedvalue']))
+            label += f'ShutterSpeed: {ss:7.4f}\n'
+        if metadata.get('photographicsensitivity', False):
+            label += 'ISO: ' + metadata['photographicsensitivity'] + '\n'
+        if metadata.get('focallength', False):
+            fl = float(Fraction(metadata['focallength']))
+            label += f'FocalLength: {fl:6.1f}\n'
+        if metadata.get('focallengthin35mmfilm', False):
+            fl = float(Fraction(metadata['focallengthin35mmfilm']))
+            label += f'FocalLength)35mm): {fl:6.1f}\n'
+        if metadata.get('fnumber', False):
+            ap = float(Fraction(metadata['fnumber']))
+            label += f'Aperture: {ap:6.1f}\n'
+        if metadata.get('exposuretime', False):
+            label += f'Exposure: {metadata['exposuretime']:s}s\n'
+        if metadata.get('make', False):
+            label += f'Make: {metadata['make']:s}\n'
+        if metadata.get('lensmodel', False):
+            label += f'Lens: {metadata['lensmodel']:s}\n'
+        if metadata.get('gpslatitude', False):
+            lat = [deg, min, sec] = metadata['gpslatitude'].split(',')
+            for i in range(len(lat)):
+                lat[i] = float(Fraction(lat[i]))
+            ref = metadata.get('gpslatituderef', '')
+            label += f'Latitude:  {lat[0]:4.0f} {lat[1]:3.0f}\' {lat[2]:4.1f}" {ref:s}\n'
+        if metadata.get('gpslongitude', False):
+            long = [deg, min, sec] = metadata['gpslongitude'].split(',')
+            for i in range(len(long)):
+                long[i] = float(Fraction(long[i]))
+            ref = metadata.get('gpslongituderef', '')
+            label += f'Longitude: {long[0]:4.0f} {long[1]:3.0f}\' {long[2]:4.1f}" {ref:s}\n'
+        if metadata.get('gpsaltitude', False):
+            alt= float(Fraction(metadata['gpsaltitude']))
+            label += f'Altitude: {alt:5.1f} m   {alt * 3.28084:5.1f} \'\n'
+
             
-            
+        print(label)
+        
         
 class Images:
     def __init__(self):
-        self.directory = '/home/jim/tools/TVSlideShow/imbedImage/'
-        self.DBx = '/home/jim/tools/TVSlideShow/makeDB/TVslides.image.test.sql'
-        self.DB = '/home/jim/tools/TVSlideShow/sql/TVslides.image.sql'
+        self.directory = '/home/jim/tools/TVSlideShow.py/'
+        #self.DBx = '/home/jim/tools/TVSlideShow.py/makeDB/TVslides.image.test.sql'
+        self.DB = '/home/jim/tools/TVSlideShow.py/sql/TVslides.sql'
         #doCmd('cp -p ' + self.DBx + ' ' + self.DB)
         #doCmd('ln ' + self.DBx + ' ' + self.DB)
         self.DBtable = 'pictures'
@@ -272,9 +323,12 @@ def main():
     picList  = pictures.getPictureFiles()
     rotates  = pictures.buildRotates()
     rotates  = pictures.flopSlides(picList)
-    pictures.countLabels(picList)
+    #pictures.countLabels(picList)
     #pprint.pprint(rotates)
-    #pictures.buildLabel('2026.06.21.All.iPhone/Yau Ma Tei, March 7, 2026/IMG_9277.jpeg')
+    meta = pictures.getLabelData('2026.06.21.All.iPhone/Yau Ma Tei, March 7, 2026/IMG_9277.jpeg')
+    pprint.pprint(meta)
+    label = pictures.composeLabel('2026.06.21.All.iPhone/Yau Ma Tei, March 7, 2026/IMG_9277.jpeg',
+                                  meta)
 
     '''
     images = Images()
