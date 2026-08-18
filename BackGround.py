743a98198f8f3c80e6be0a61645390f787f41a41
Jim Mollmann <jim.mollmann@gmail.com>
2026-08-18 14:54:20 -0400

    Initial pass at using the Raspberry Pi display code to set workspace backgound images on my desktop

---
+++ BackGround.py
@@ -0,0 +1,302 @@
+#!/usr/bin/python3 -u
+
+
+# Jim Mollmann
+import datetime
+import time
+import math
+import sys
+import getopt
+import os
+import subprocess
+import string
+import sched
+import random
+import argparse
+import sqlite3
+import pprint
+
+class Picture:
+    # select and get an image
+    def __init__(self):
+        self.directory = '/home/jim/bin/Slides/'
+        self.srcIP     = '192.168.123.4'
+        self.DB        = '/home/jim/tools/TVSlideShow.py/TVSlides.sql'
+        self.DBtable   = 'pictures'
+        self.DBmtime   = {'sql.new' : 0, 'sql.old' : 0, 'sql' : 0}
+        self.myDBmtime = 0
+        self.GetDB()
+        self.myDBmtime = self.DBmtime['sql']
+        self.db        = sqlite3.connect(self.DB)
+        self.c         = self.db.cursor()
+        countrows      = 'SELECT COUNT(*) FROM ' + self.DBtable + ';'
+        self.c.execute(countrows)
+        self.Rows      = self.c.fetchone()[0]
+        print('rows:', self.Rows)
+        #self.rowsleft  = list(range(1, self.Rows))
+        self.n         = 0
+        self.groupSize = 10
+        self.group     = []
+        self.rows      = {}
+        self.files     = {}
+        self.totalDirs = 0
+        self.debug     = False
+
+    def GetDB(self):
+        # just restart if the database is newer
+        pass
+        '''
+        restart = False
+        debug = False
+        DBbase = '.'.join(self.DB.split('.')[0 : -1]) + '.'
+        for suffix in self.DBmtime.keys():
+            db = DBbase + suffix
+            if os.path.exists(db):
+                self.DBmtime[suffix] = os.path.getmtime(db)
+            else:
+                self.DBmtime[suffix] = 0
+            if debug: print(DBbase + suffix, ':', self.DBmtime[suffix])
+        curDB = DBbase + 'sql'
+        oldDB = curDB + '.old'
+        newDB = curDB + '.new'
+        if self.myDBmtime and self.DBmtime['sql'] > self.myDBmtime:
+            print('DB updated:', curDB)
+            restart = True
+        elif self.DBmtime['sql.new'] > self.DBmtime['sql']:
+            print(newDB, ' is newer than ', curDB)
+            restart = True
+            if os.path.exists(oldDB):
+                command = 'rm ' + oldDB
+                command = 'mv ' + oldDB + '.gone'
+                doCmd(command, debug)
+            command = ' '.join(['mv', curDB, oldDB])
+            doCmd(command, debug)
+            command = ' '.join(['mv', newDB, curDB])
+            doCmd(command, debug)
+        else:
+            pass
+        with open('/proc/' + str(os.getpid()) + '/cmdline', 'r') as cmdline:
+            myCmd = cmdline.read()
+        if restart:
+            args = myCmd[0:-1].split('\0')
+            path = args.pop(0)
+            py = os.path.abspath(__file__)
+            print('DB updated, restarting')
+            os.execv(py, args)
+        '''
+
+    def getRange(self, idx, length):
+        if length < self.groupSize:
+            return 0, length - 1
+        around = int(self.groupSize / 2)
+        first = idx - around
+        last  = idx + around
+        if first < 0:
+            last  = last - first
+            first = 0
+        if last > length - 1:
+            first = first - (last - length)
+            last  = length
+        if self.debug:
+            print('getRange: idx:', idx, 'length:', length, \
+                  'first:', first, 'last:', last)
+        return first, last
+
+    def Get1Picture(self):
+        self.n += 1
+        # list idicies
+        ROWNUM   = 0
+        DIRFILE  = 1
+        DIRNUM   = 0
+        FILENUM  = 1
+
+        if len(self.rows) == 0:
+            # build "DB" of pictures
+            select = 'SELECT dirNum, fileNum, filename FROM ' + \
+                self.DBtable + ';'
+            self.c.execute(select)
+            rowNum = 0
+            for row in self.c:
+                dirNum, fileNum, filename = row
+                self.rows[rowNum] = [dirNum, fileNum]
+                if dirNum not in self.files:
+                    self.files[dirNum] = {}
+                self.files[dirNum][fileNum] = {'rowNum' : rowNum, 'filename' : filename}
+                rowNum += 1
+            self.totalDirs = len(self.files)
+
+        if len(self.group) == 0:
+            #check for a new DB
+            self.GetDB()
+            # need a new group of pictures to show
+            selection = random.randint(0, len(self.rows) - 1)
+            # sel = [ rowNum, [ dirNum, fileNum ] ]
+            sel = list(sorted(self.rows.items()))[selection]
+            if self.debug:
+                print('sel:', sel)
+            rowNum  = sel[ROWNUM]
+            dirNum  = sel[DIRFILE][DIRNUM]
+            fileNum = sel[DIRFILE][FILENUM]
+            if self.debug:
+                print(selection, rowNum, dirNum, fileNum)
+            i = 0
+            for fileNo in self.files[dirNum]:
+                if fileNo == self.rows[rowNum][FILENUM]:
+                    myIdx = i
+                    break
+                i += 1
+            myDirLen = len(self.files[self.rows[rowNum][DIRNUM]])
+            first, last = self.getRange(myIdx, myDirLen)
+            i = 0
+            deletes = []
+            for fileNo in self.files[dirNum]:
+                if i >= first and i <= last:
+                    self.group.append(self.files[dirNum][fileNo]['filename'])
+                    rowNum = self.files[dirNum][fileNo]['rowNum']
+                    deletes.append([rowNum, dirNum, fileNo])
+                i += 1
+            if self.debug:
+                print('deletes:', deletes)
+            for rowNum, dirNum, fileNum in deletes:
+                del self.files[dirNum][fileNum]
+                if len(self.files[dirNum]) == 0:
+                    del self.files[dirNum]
+                del self.rows[rowNum]
+            if self.debug:
+                print('group:', self.group)
+
+        filename = self.group.pop(0)
+        select = 'SELECT image FROM ' + self.DBtable + ' WHERE filename = ? ;'
+        self.c.execute(select, (filename,))
+        (image,) = self.c.fetchone()
+        when = datetime.datetime.now().replace(microsecond = 0)
+        print(f'{str(when):^19s}: {self.n:6d} {filename:s}')
+        self.showStats(when)
+        return (filename, image)
+
+    def showStats(self, when):
+        if when.minute > 0:
+            return
+        picsLeft = 0
+        for dir in self.files:
+            picsLeft += len(self.files[dir])
+        picsLeft += len(self.group)
+        print(f'{str(when):^19s}: {picsLeft:6d} of {self.Rows:6d} pictures remain')
+        print(f'{" ":^19s}: {len(self.files):6d} of {self.totalDirs:6d} directories remain')
+
+
+class Slide:
+    # build & display image for screen
+    # new image every 5 minutes, if needed
+    def __init__(self, workspace):
+        #self.resolution = "1920x1080"
+        self.pictures   = Picture()
+        #self.VolumeUp   = True
+        self.directory  = '/home/jim/tools/TVSlideShow.py/images/'
+        self.workspace  = workspace
+        self.idx        = 0
+        self.debug      = False
+
+    def Show1Slide(self):
+        (filename, image) = self.pictures.Get1Picture()
+        #print('Show1Slide', filename)
+        if image is None:
+            print('Image is "None". Skipping.')
+            return
+        self.idx += 1
+        idx = self.idx % 5
+        wksp = f'{self.workspace:d}'
+        displayFile = self.directory + 'display.' + str(idx) + wksp +'.jpg'
+        resize = 'magick - -resize 3640x2088 -quality 95 jpeg:-'
+        result = doCmd(resize, input = image)
+        if result.returncode != 0:
+            print('ERROR: Show1Slide resize image:', filename)
+        else:
+            image = result.stdout
+        with open(displayFile, 'wb') as display_file:
+            size = display_file.write(image)
+        #
+	# run "xfconf-query -c xfce4-desktop -m" from the CLI
+	# use the GUI to change the desktop background and note the name
+	# turn off "apply to all workspaces
+	#
+        if self.debug:
+            command = 'display ' + displayFile
+        else:
+            command = 'xfconf-query -c xfce4-desktop ' +            \
+                '-p /backdrop/screen0/monitorHDMI-0/workspace' + wksp + \
+                '/last-image -s ' + displayFile
+        #print(command)
+        doCmd(command)
+
+class SlideTimer:
+    # handle when to display a picture
+    def __init__(self, scheduler, workspace):
+        self.showSlide = True
+        self.scheduler = scheduler
+        self.frequency = 5 * 60
+        self.starttime = 0
+        self.slides    = Slide(workspace)
+        self.workspace = workspace
+
+    def Schedule(self, frequency = None):
+        if frequency:
+            self.frequency = frequency
+        now = datetime.datetime.now()
+        firstTime = now.replace(hour = 0, minute = 0, second = 0, microsecond = 0) -\
+                    datetime.timedelta(weeks = 1)
+        while firstTime < now:
+            firstTime += datetime.timedelta(seconds = self.frequency)
+        self.starttime = firstTime
+        print('showSlide Start time:', self.starttime, 'workspace:',self.workspace)
+        self.scheduler.enterabs(time.mktime(self.starttime.timetuple()), 1,
+                                self.ShowSlide,
+                                kwargs = {'WKSP': self.workspace})
+
+    def ShowSlide(self, WKSP = None):
+        self.starttime = self.starttime + datetime.timedelta(seconds = self.frequency)
+        self.scheduler.enterabs(time.mktime(self.starttime.timetuple()), 1,
+                                self.ShowSlide,
+                                kwargs = {'WKSP': self.workspace})
+        self.slides.Show1Slide()
+        
+def doCmd(command, printFailure = True, debug = False, input = None):
+    if debug:
+        print('doCmd:command:', command)
+    result = subprocess.run(command, shell = True, stdout = subprocess.PIPE, \
+                            stderr=subprocess.PIPE, input = input, check = False)
+    if debug:
+        if result.stdout is not None:
+            print('stdout:' + '\n' + result.stdout.decode('utf-8'))
+        if result.stderr is not None:
+            print('stderr:' + '\n' + result.stderr.decode('utf-8'))
+
+    if result.returncode != 0 & printFailure:
+        #print(result.stdout)
+        print('Failed: RC:', result.returncode, command)
+        print(result.stderr)
+    if debug:
+        print('doCmd:result:', result)
+    return result
+
+def main():
+    scheduler = sched.scheduler(time.time, time.sleep)
+    #power = Power(scheduler)
+    for workspace in range(7,9):
+        slides = SlideTimer(scheduler, workspace)
+        slides.Schedule(frequency = 1 * 60)
+    #power.Schedule()
+
+    print(len(scheduler.queue))
+    #print(scheduler.queue)
+    for event in scheduler.queue:
+        print('Next:', datetime.datetime.fromtimestamp(event.time).replace(microsecond = 0),
+              'Action:', str(event.action).split(' ')[2],
+              'Pri:', event.priority,
+              'Arg:', event.argument,
+              'WKSP:', event.kwargs['WKSP'])
+
+    scheduler.run()
+
+if __name__ == '__main__':
+  main()
